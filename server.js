1cb534d7c54175ea1145f91b6c5ee759bb041bf1
luanaggoncalves <luanaggoncalves98@gmail.com>
2022-04-03 11:06:37 -0300

    create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// primeira rota
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+// configura a porta, inicia o servidor.
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});

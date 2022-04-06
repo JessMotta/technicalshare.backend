2a42ae20f371ac5318e9bf2b67be1ec32e76c84c
luanaggoncalves <luana.ggoncalves98@gmail.com>
2022-04-06 14:36:39 -0300

    remove console.log

--- app/models/index.js
+++ app/models/index.js
@@ -22,7 +22,6 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    console.log('file: ', file);
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });

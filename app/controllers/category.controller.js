9c59a6f74160756dee06b08f2284cac72aaa8ca1
luanaggoncalves <luana.ggoncalves98@gmail.com>
2022-04-09 20:44:09 -0300

    create category controller: Create, Read

---
+++ app/controllers/category.controller.js
@@ -0,0 +1,20 @@
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { categoryService } = require('../services');
+
+const createCategory = catchAsync(async (req, res) => {
+  const category = await categoryService.createCategory(req.body);
+  res.status(httpStatus.CREATED).send(category);
+});
+
+const getCategories = catchAsync(async (req, res) => {
+  const result = await categoryService.getCategories();
+  res.send(result);
+});
+
+
+module.exports = {
+  createCategory,
+  getCategories,
+};

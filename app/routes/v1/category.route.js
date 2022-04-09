2c8f80a4a4953e28a2aa8572780214956f74f7ab
luanaggoncalves <luana.ggoncalves98@gmail.com>
2022-04-09 20:46:42 -0300

    Add Swagger documentation for categories

--- app/routes/v1/category.route.js
+++ app/routes/v1/category.route.js
@@ -16,3 +16,144 @@ router
 
 module.exports = router;
 
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: Categories are technical skillsets. Users can have multiple categories.
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Creates a category
+ *     description: This endpoint creates a new category if its name is unique.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: DevOps
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateName'
+ *
+ *   get:
+ *     summary: Gets all categories
+ *     description: This endpoint returns all categories. The categories are sorted by creation date.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Category'
+ */
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *   get:
+ *     summary: Finds a category by id. Returns a category if it exists.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Updates a category by id
+ *     description: This endpoint updates a category by id. If the category name is unique, the category is updated.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: Blockchain
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateName'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Deletes a category by id. Empty response.
+ *     description: This endpoint deletes a category by id if it exists.
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     responses:
+ *       "204":
+ *         description: No content
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
